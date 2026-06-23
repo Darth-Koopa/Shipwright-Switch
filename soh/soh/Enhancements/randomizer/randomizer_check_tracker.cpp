20ff6e267302168b81eb82500003365e3e826f4e
wonderfulnx <xin.na16@outlook.com>
2026-06-24 03:17:15 +0800

    Finished chinese-support code editing
    Updated some tool scripts, and apply changes to Shipwright repo,
    including some handwriten changes and some script-generated changes.

--- soh/soh/Enhancements/randomizer/randomizer_check_tracker.cpp
+++ soh/soh/Enhancements/randomizer/randomizer_check_tracker.cpp
@@ -1910,7 +1910,7 @@ void DrawLocation(RandomizerCheck rc) {
                 } else {
                     if (IsHeartPiece((GetItemID)Rando::StaticData::RetrieveItem(loc->GetVanillaItem()).GetItemID())) {
                         if (gSaveContext.language == LANGUAGE_ENG || gSaveContext.language == LANGUAGE_GER ||
-                            gSaveContext.language == LANGUAGE_JPN) {
+                            gSaveContext.language == LANGUAGE_JPN || gSaveContext.language == LANGUAGE_CHI) {
                             txt = Rando::StaticData::RetrieveItem(loc->GetVanillaItem()).GetName().english;
                         } else if (gSaveContext.language == LANGUAGE_FRA) {
                             txt = Rando::StaticData::RetrieveItem(loc->GetVanillaItem()).GetName().french;
@@ -1943,7 +1943,7 @@ void DrawLocation(RandomizerCheck rc) {
                 } else {
                     if (IsHeartPiece((GetItemID)Rando::StaticData::RetrieveItem(loc->GetVanillaItem()).GetItemID())) {
                         if (gSaveContext.language == LANGUAGE_ENG || gSaveContext.language == LANGUAGE_GER ||
-                            gSaveContext.language == LANGUAGE_JPN) {
+                            gSaveContext.language == LANGUAGE_JPN || gSaveContext.language == LANGUAGE_CHI) {
                             txt = Rando::StaticData::RetrieveItem(loc->GetVanillaItem()).GetName().english;
                         } else if (gSaveContext.language == LANGUAGE_FRA) {
                             txt = Rando::StaticData::RetrieveItem(loc->GetVanillaItem()).GetName().french;
